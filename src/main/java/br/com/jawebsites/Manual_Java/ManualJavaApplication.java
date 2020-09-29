bf64d64b6c78f1b090240ff169141ea7974d132b
Andre <falconi879@gmail.com>
2020-09-29 11:15:16 -0300

    Criação do projeto

---
+++ src/main/java/br/com/jawebsites/Manual_Java/ManualJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.jawebsites.Manual_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManualJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManualJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jawebsites/Manual_Java/ManualJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jawebsites.Manual_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManualJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
